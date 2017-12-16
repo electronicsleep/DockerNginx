b4d6a90896f744e624c08620f44e3a459898f4e3
Chris <electronicsleep@gmail.com>
2017-12-16 00:59:46 -0800

    PHP7/Nginx/Docker

--- docker-php7-nginx.sh
+++ docker-php7-nginx.sh
@@ -3,6 +3,5 @@ echo "Starting PHP7"
 service php7.0-fpm start
 echo "Starting Nginx"
 echo "Location: http://docker.local:8000/index.html"
-echo "Location: http://docker.local:8000/phpinfo.php"
-echo "Location: http://docker.local:8000/chris.php"
+echo "Location: http://docker.local:8000/hello.php"
 /usr/sbin/nginx -g "daemon off;"
